5b5cbf1da4a2b96cf59d8f14f4636b488f0cddbe
comar80 <comar80@gmail.com>
2024-10-10 17:47:34 -0300

    autenticacao

--- src/main/java/br/com/fiap/trafego/config/security/SecurityConfig.java
+++ src/main/java/br/com/fiap/trafego/config/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/*").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/*").permitAll()
                         .requestMatchers(HttpMethod.DELETE, "/api/*").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/*").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/api/*").hasAnyRole("ADMIN", "USER")
