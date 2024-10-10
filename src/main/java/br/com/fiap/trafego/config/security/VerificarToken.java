28fec137fda9de91705c8abc03efa52c202da557
comar80 <comar80@gmail.com>
2024-10-10 14:06:34 -0300

    teste CD 2

--- src/main/java/br/com/fiap/trafego/config/security/VerificarToken.java
+++ src/main/java/br/com/fiap/trafego/config/security/VerificarToken.java
@@ -1,54 +1,54 @@
-package br.com.fiap.trafego.config.security;
+// package br.com.fiap.trafego.config.security;
 
-import br.com.fiap.trafego.repository.UsuarioRepository;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+// import br.com.fiap.trafego.repository.UsuarioRepository;
+// import jakarta.servlet.FilterChain;
+// import jakarta.servlet.ServletException;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+// import org.springframework.security.core.context.SecurityContextHolder;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-@Component
-public class VerificarToken extends OncePerRequestFilter {
+// @Component
+// public class VerificarToken extends OncePerRequestFilter {
 
-    @Autowired
-    private TokenService tokenService;
+//     @Autowired
+//     private TokenService tokenService;
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+//     @Autowired
+//     private UsuarioRepository usuarioRepository;
 
-    @Override
-    protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain) throws ServletException, IOException {
+//     @Override
+//     protected void doFilterInternal(
+//             HttpServletRequest request,
+//             HttpServletResponse response,
+//             FilterChain filterChain) throws ServletException, IOException {
 
-        String authorizationHeader = request.getHeader("Authorization");
-        String token = "";
+//         String authorizationHeader = request.getHeader("Authorization");
+//         String token = "";
 
-        if (authorizationHeader == null){
-            token = null;
-        } else {
-            token = authorizationHeader.replace("Bearer", "").trim();
-            String login = tokenService.validarToken(token);
-            UserDetails usuario = usuarioRepository.findByEmail(login);
+//         if (authorizationHeader == null){
+//             token = null;
+//         } else {
+//             token = authorizationHeader.replace("Bearer", "").trim();
+//             String login = tokenService.validarToken(token);
+//             UserDetails usuario = usuarioRepository.findByEmail(login);
 
-            UsernamePasswordAuthenticationToken authentication =
-                    new UsernamePasswordAuthenticationToken(
-                            usuario,
-                            null,
-                            usuario.getAuthorities()
-                    );
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
+//             UsernamePasswordAuthenticationToken authentication =
+//                     new UsernamePasswordAuthenticationToken(
+//                             usuario,
+//                             null,
+//                             usuario.getAuthorities()
+//                     );
+//             SecurityContextHolder.getContext().setAuthentication(authentication);
+//         }
 
-        filterChain.doFilter(request, response);
+//         filterChain.doFilter(request, response);
 
-    }
-}
+//     }
+// }
--- src/test/java/br/com/fiap/trafego/TrafegoApplicationTests.java
+++ src/test/java/br/com/fiap/trafego/TrafegoApplicationTests.java
@@ -1,4 +1,56 @@
-// 
+// package br.com.fiap.trafego;
+
+// import static org.mockito.ArgumentMatchers.*;
+// import static org.mockito.BDDMockito.*;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+// import org.junit.jupiter.api.DisplayName;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+// import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.test.web.servlet.ResultActions;
+// import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+// import br.com.fiap.trafego.dto.UsuarioCadastroDTO;
+// import br.com.fiap.trafego.service.UsuarioService;
+
+// @WebMvcTest
+// public class TrafegoApplicationTests {
+
+// 	@Autowired
+//     private MockMvc mockMvc;
+
+// 	@MockBean
+// 	private UsuarioService service;
+
+// 	// private UsuarioCadastroDTO usuarioCadastroDTO;
+
+// 	// @DisplayName("test create usuario")
+//     // @Test
+//     // void testGivenNewUsuario_CreateUsuario() throws Exception{
+        
+//     //     given(service.salvarUsuario(any(UsuarioCadastroDTO.class)))
+//     //         .willAnswer((invocation) -> invocation.getArguments()[0]);
+
+//     //     String body = "{\"nome\":\"User1\",\"email\":\"email@email.com\",\"senha\":\"senha123\",\"role\":\"admin\"}";
+        
+//     //     ResultActions response = mockMvc.perform(MockMvcRequestBuilders.post("/api/usuarios")
+//     //         .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+//     //         .content(body));
+
+//     //     response.andExpect(status().isCreated());
+//     // }
+
+//     @DisplayName("test fail")
+//     @Test
+//     void testGivenNewUser_whenCreate_thenFail() throws Exception{
+//         // fail("Um erro acontecerá");
+//     }
+
+// }
+
 
 package br.com.fiap.trafego;
 
