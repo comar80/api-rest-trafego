9b541f54aeb41d03fe793e414ee3d810720f3a90
comar80 <comar80@gmail.com>
2024-10-11 15:35:32 -0300

    testes

--- src/main/java/br/com/fiap/trafego/config/security/VerificarToken.java
+++ src/main/java/br/com/fiap/trafego/config/security/VerificarToken.java
@@ -1,51 +1,54 @@
 package br.com.fiap.trafego.config.security;
 
+import br.com.fiap.trafego.repository.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import br.com.fiap.trafego.repository.UsuarioRepository;
-
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
 public class VerificarToken extends OncePerRequestFilter {
 
-    private final TokenService tokenService;
-    private final UsuarioRepository usuarioRepository;
+    @Autowired
+    private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoveryToken(request);
-        if (token != null) {
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
 
-            var userName = tokenService.validateToken(token);
-            UserDetails usuario = usuarioRepository.findByEmail(userName);
+        String authorizationHeader = request.getHeader("Authorization");
+        String token = "";
 
+        if (authorizationHeader == null){
+            token = null;
+        } else {
+            token = authorizationHeader.replace("Bearer", "").trim();
+            String login = tokenService.validarToken(token);
+            UserDetails usuario = usuarioRepository.findByEmail(login);
 
-            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            usuario,
+                            null,
+                            usuario.getAuthorities()
+                    );
             SecurityContextHolder.getContext().setAuthentication(authentication);
-
         }
-            filterChain.doFilter(request, response);
 
-    }
+        filterChain.doFilter(request, response);
 
-    private String recoveryToken(HttpServletRequest request) {
-        var authHeader = request.getHeader("Authorization");
-
-        if (authHeader == null) {
-            return null;
-        }
-        return authHeader.replace("Bearer ", "");
     }
 }
